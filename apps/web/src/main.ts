6070fb7dff1632f3c6316aab1bd68e1df6b83df7
dieWehmut <diesehnsucht0@gmail.com>
2026-08-20 18:46:19 +0800

    feat(web): install isolated Pinia container

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -8,8 +8,9 @@ import App from './App.vue'
 import { appI18n } from './i18n'
 import { appRouter } from './router'
 import { createAppStores } from './stores/app'
+import { createAppPinia } from './stores/pinia'
 
 const app = createApp(App)
 const stores = createAppStores()
 
-app.use(appI18n).use(appRouter).use(stores).mount('#app')
+app.use(appI18n).use(appRouter).use(createAppPinia()).use(stores).mount('#app')
